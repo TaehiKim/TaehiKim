e2e967f139a2736a9856a93f9c8477fd9bbd6f1e
TaehiKim <taehikim6234@gmail.com>
2022-01-04 22:30:42 -0600

    playing around with angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeMessageComponent } from './welcome-message/welcome-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeMessageComponent
   ],
   imports: [
     BrowserModule,
